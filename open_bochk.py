6f137b01165118c0eb49c6b4df42f9cab4caa6cb
ZhangSteven <steven.shutao@gmail.com>
2016-11-26 23:13:39 +0800

    read_bochk() tested, error conditions not tested yet

--- open_bochk.py
+++ open_bochk.py
@@ -11,21 +11,22 @@ import datetime
 from bochk.utility import logger, get_datemode, retrieve_or_create
 
 
+class InvalidHoldingType(Exception):
+	pass
 
 class InvalidFieldName(Exception):
 	pass
 
+class InconsistentPosition(Exception):
+	pass
 
-
-class InconsistentSubtotal(Exception):
+class InconsistentHolding(Exception):
 	pass
 
 
 
 def read_bochk(filename, port_values):
-	"""
-	
-	"""
+
 	logger.debug('in read_bochk()')
 
 	wb = open_workbook(filename=filename)
@@ -36,6 +37,9 @@ def read_bochk(filename, port_values):
 		row = row + 1
 
 	fields = read_fields(ws, row)
+
+	grand_total = read_holdings(ws, row+1, port_values, fields)
+	validate_all_holdings(port_values['holdings'], grand_total)
 
 	logger.debug('out of read_bochk()')
 
@@ -65,7 +69,6 @@ def read_fields(ws, row):
 	fields: the list of data fields
 	"""
 	d = {
-		'Record Type':'record_type',
 		'Generation Business Date':'generation_date',
 		'Statement Date':'statement_date',	
 		'Custody Account Name':'account_name',
@@ -77,7 +80,7 @@ def read_fields(ws, row):
 		'Securities name':'security_name',
 		'Quantity Type':'quantity_type',
 		'Holding':'holding_quantity',
-		'Mnemonic Name':'holding_status',	
+		'Mnemonic Name':'holding_type',	
 		'Market Price Currency':'market_price_currency',
 		'Market Unit Price':'market_price',
 		'Market Value':'market_value',
@@ -87,7 +90,7 @@ def read_fields(ws, row):
 		'Equivalent Market Value':'equivalent_market_value'
 	}
 
-	column = 0
+	column = 1
 	fields = []
 
 	while column < 21:
@@ -97,8 +100,288 @@ def read_fields(ws, row):
 		else:
 			logger.error('read_fields(): invalid column name {0} at row {1}, column {2}'.
 							format(cell_value, row, column))
-			raise InvalidFieldName
+			raise InvalidFieldName()
 
 		column = column + 1
 
 	return fields
+
+
+
+def read_holdings(ws, row, port_values, fields):
+	"""
+	Read the holdings, line by line, until it reaches
+
+	1. The ALL section, if it exists, or,
+	2. The end of the holdings.
+
+	Returns the row number where the reading stops, i.e., the next
+	line after all the positions.
+	"""
+	logger.debug('read_holdings(): at row {0}'.format(row))
+
+	holdings = []
+	port_values['holdings'] = holdings
+
+	while row < ws.nrows:
+		if is_grand_total(ws, row):
+			return read_grand_total(ws, row)
+
+		elif is_blank_line(ws, row):
+			return None
+
+		position = {}
+		row = read_position(ws, row, fields, position)
+		validate_position(position)
+		holdings.append(position)
+
+	# end of while loop
+
+
+
+def read_position(ws, row, fields, position):
+	"""
+	Read a position starting at the row number, stops at the
+	next position.
+
+	Returns the row number of next line after this position.
+	"""
+	initialize_position(position)
+
+	i = 0
+	cell_value = ws.cell_value(row, 0)
+	record_type = cell_value.strip()
+
+	while row < ws.nrows:
+		if record_type == 'Holding Details':
+			read_position_holding_detail(ws, row, fields, position)
+		elif record_type == 'Sub-Total':
+			read_position_sub_total(ws, row, fields, position)
+		elif record_type == 'Available Balance':
+			read_position_available_balance(ws, row, fields, position)
+			break
+
+		row = row + 1
+		cell_value = ws.cell_value(row, 0)
+		record_type = cell_value.strip()
+	# end of while loop
+
+	return row+1
+
+
+
+def initialize_position(position):
+	position['settled_units'] = 0
+	position['pending_receipt'] = 0
+	position['pending_delivery'] = 0
+
+
+
+def read_position_holding_detail(ws, row, fields, position):
+	"""
+	Read the holding details part of a position.
+	"""
+	i = 1
+
+	for fld in fields:
+		cell_value = ws.cell_value(row, i)
+		if fld == 'generation_date' or fld == 'statement_date':
+			position[fld] = xldate_as_datetime(cell_value, get_datemode())
+		elif fld == 'holding_quantity':
+			if isinstance(cell_value, float):
+				holding_quantity = cell_value
+			else:
+				logger.error('read_position_holding_detail(): holding_quantity is not of type float, at row {0}, column {1}'.
+								format(row, i))
+				raise TypeError
+
+		else:
+			value = cell_value.strip()
+			if fld == 'holding_type':
+				if value == 'NOM':
+					position['settled_units'] = holding_quantity
+				elif value == 'PENDING DELIVERY':
+					position['pending_delivery'] = position['pending_delivery'] + holding_quantity
+				elif value == 'PENDING RECEIPT':
+					position['pending_receipt'] = position['pending_receipt'] + holding_quantity
+				else:
+					logger.error('read_position_holding_detail(): invalid holding type encountered, type={0}, at row {1}'.
+									format(position['holding_type'], row))
+					raise InvalidHoldingType()
+
+				break
+
+			else:
+				position[fld] = value
+
+		i = i + 1
+
+
+
+def read_position_sub_total(ws, row, fields, position):
+	"""
+	Read the sub-total part of a position
+	"""
+	i = 1
+
+	for fld in fields:
+		cell_value = ws.cell_value(row, i)
+
+		if fld in ['market_price_currency', 'exchange_currency_pair', 'equivalent_currency']:
+			position[fld] = cell_value.strip()
+		elif fld in ['holding_quantity', 'market_price', 'market_value', 'exchange_rate', 'equivalent_market_value']:
+			if not isinstance(cell_value, float):
+				logger.error('read_position_sub_total(): {0} is not of type float, at row {1}, column {2}'.
+								format(fld, row, i))
+				raise TypeError
+
+			if fld == 'holding_quantity':
+				position['sub_total'] = cell_value
+			else:
+				position[fld] = cell_value
+
+		if position['account_number'] == 'All' and fld == 'market_value':
+			break
+
+		i = i + 1
+
+
+
+def read_position_available_balance(ws, row, fields, position):
+	"""
+	Read the available balance part of the position.
+	"""
+	i = 1
+	for fld in fields:
+		cell_value = ws.cell_value(row, i)
+		if fld == 'holding_quantity':
+			if not isinstance(cell_value, float):
+				logger.error('read_position_available_balance(): available balance is not of type float, at row {1}, column {2}'.
+								format(row, i))
+				raise TypeError
+
+			position['available_balance'] = cell_value
+
+		i = i + 1
+
+
+
+def validate_position(position):
+	# assume quantity do not have decimal places.
+	x = position['settled_units'] - position['pending_delivery'] + \
+		position['pending_receipt'] - position['sub_total']
+
+	y = position['settled_units'] - position['pending_delivery'] - \
+		position['available_balance']
+
+	if position['quantity_type'] == 'FAMT':
+		z = position['sub_total']*position['market_price']/100 - position['market_value']
+	else:
+		z = position['sub_total']*position['market_price'] - position['market_value']
+
+	if 'exchange_rate' in position:
+		z2 = position['market_value']*position['exchange_rate'] - position['equivalent_market_value']
+	else:
+		z2 = 0
+
+
+	if x==0 and y==0 and abs(z) < 0.01 and abs(z2) < 0.01:
+		pass
+	else:
+		logger.error('validate_position(): inconsistent position: settled={0}, pending_delivery={1}, pending_receipt={2}, sub_total={3}, available={4}'.
+						format(position['settled_units'], position['pending_delivery'],
+								position['pending_receipt'], position['sub_total'], position['available_balance']))
+		raise InconsistentPosition()
+
+
+
+def is_grand_total(ws, row):
+	cell_value = ws.cell_value(row, 0)
+	if cell_value.strip() == 'Grand Total of All Custody A/C':
+		return True
+	else:
+		return False
+
+
+
+def read_grand_total(ws, row):
+	cell_value = ws.cell_value(row, 20)
+	if isinstance(cell_value, float):
+		return cell_value
+	else:
+		logger.error('read_grand_total(): grand total is not of type float, at row {0}, column 20'.format(row))
+
+
+
+def is_blank_line(ws, row):
+	for i in range(5):
+		cell_value = ws.cell_value(row, i)
+
+		if not isinstance(cell_value, str) or not cell_value.strip() == '':
+			return False
+
+	return True
+
+
+
+def validate_all_holdings(holdings, grand_total):
+	temp_dict = {}
+	key_position_fields = ['settled_units', 'pending_receipt', 'pending_delivery', 
+							'sub_total', 'available_balance']
+
+	for position in holdings:
+		key = (position['market_code'], position['security_id_type'], position['security_id'])
+		if position['account_number'] == 'All':
+			logger.debug('validate_all_holdings(): validate security {0}'.format(position['security_id']))
+			if not is_position_fields_consistent(temp_dict[key], position, key_position_fields):
+				logger.error('validate_all_holdings(): inconsistent positions: {0}'.format(key))
+				raise InconsistentPosition()
+		else:
+			if key in temp_dict:
+				merge_position_fields(temp_dict[key], position, key_position_fields)
+			else:
+				temp_dict[key] = copy_position_fields(position, key_position_fields)
+
+	if not grand_total is None:
+		grand_total_position = accumulate_position_total(holdings)
+		if abs(grand_total_position - grand_total) > 0.01:
+			logger.error('validate_all_holdings(): inconsistent grand_total: position total={0}, grand total={1}'.
+							format(grand_total_position, grand_total))
+			raise InconsistentPosition()
+
+
+
+def is_position_fields_consistent(position1, position2, key_position_fields):
+	# Here we assume all holding quantity are of integer value, e.g.
+	# quantity 123, 123.0 are fine, but 123.1 are not. Because float
+	# comparison is different.
+	for fld in key_position_fields:
+		if not position1[fld] == position2[fld]:
+			return False
+
+	return True
+
+
+
+def merge_position_fields(position1, position2, key_position_fields):
+	for fld in key_position_fields:
+		position1[fld] = position1[fld] + position2[fld]
+
+
+
+def copy_position_fields(position, key_position_fields):
+	new_position = {}
+	for fld in key_position_fields:
+		new_position[fld] = position[fld]
+
+	return new_position
+	
+
+
+def accumulate_position_total(holdings):
+	t = 0
+	for position in holdings:
+		if position['account_number'] != 'All':
+			t = t + position['equivalent_market_value']
+
+	return t
--- test/test_bochk.py
+++ test/test_bochk.py
@@ -3,10 +3,14 @@ Test the open_jpm.py
 """
 
 import unittest2
-import datetime
+from datetime import datetime
 from xlrd import open_workbook
 from bochk.utility import get_current_path
-from bochk.open_bochk import field_begins, read_fields
+from bochk.open_bochk import field_begins, read_fields, initialize_position, \
+                                read_position_holding_detail, read_position_sub_total, \
+                                read_position_available_balance, read_position, \
+                                validate_position, is_grand_total, read_grand_total, \
+                                read_holdings, read_bochk
 
 
 
@@ -31,10 +35,16 @@ class TestBOCHK(unittest2.TestCase):
 
 
 
-    def test_field_begins(self):
-        filename = get_current_path() + '\\samples\\sample_holdings2.xls'
+    def get_worksheet(self, filename):
+        filename = get_current_path() + filename
         wb = open_workbook(filename=filename)
         ws = wb.sheet_by_index(0)
+        return ws
+
+
+
+    def test_field_begins(self):
+        ws = self.get_worksheet('\\samples\\sample_holdings2.xls')
         row = 0
         self.assertFalse(field_begins(ws, row))
         row = 2
@@ -45,16 +55,337 @@ class TestBOCHK(unittest2.TestCase):
         """
         Read the date
         """
-        filename = get_current_path() + '\\samples\\sample_holdings2.xls'
-        wb = open_workbook(filename=filename)
-        ws = wb.sheet_by_index(0)
+        ws = self.get_worksheet('\\samples\\sample_holdings2.xls')
         row = 0
 
         while not field_begins(ws, row):
             row = row + 1
 
         fields = read_fields(ws, row)
-        self.assertEqual(len(fields), 21)
-        self.assertEqual(fields[0], 'record_type')
-        self.assertEqual(fields[11], 'holding_quantity')
-        self.assertEqual(fields[20], 'equivalent_market_value')
+        self.assertEqual(len(fields), 20)
+        self.assertEqual(fields[0], 'generation_date')
+        self.assertEqual(fields[10], 'holding_quantity')
+        self.assertEqual(fields[19], 'equivalent_market_value')
+
+
+
+    def test_read_position_holding_detail(self):
+        ws = self.get_worksheet('\\samples\\sample_holdings2.xls')
+        fields = read_fields(ws, 2)
+
+        position = {}
+        initialize_position(position)
+        read_position_holding_detail(ws, 3, fields, position)
+
+        self.assertEqual(len(position), 13)
+        self.assertEqual(position['generation_date'], datetime(2016,11,16))
+        self.assertEqual(position['statement_date'], datetime(2016,11,16))
+        self.assertEqual(position['security_id_type'], 'ISIN')
+        self.assertEqual(position['security_id'], 'US09681MAC29')
+        self.assertEqual(position['settled_units'], 7200000)
+
+
+
+    def test_read_position_holding_detail2(self):
+        ws = self.get_worksheet('\\samples\\sample_holdings3.xls')
+        fields = read_fields(ws, 2)
+
+        position = {}
+        initialize_position(position)
+        read_position_holding_detail(ws, 20, fields, position)
+        read_position_holding_detail(ws, 21, fields, position)
+
+        self.assertEqual(len(position), 13)
+        self.assertEqual(position['generation_date'], datetime(2016,7,12))
+        self.assertEqual(position['statement_date'], datetime(2016,7,11))
+        self.assertEqual(position['security_id_type'], 'ISIN')
+        self.assertEqual(position['security_id'], 'CNE1000021L3')
+        self.assertEqual(position['settled_units'], 0)
+        self.assertEqual(position['pending_delivery'], 222000)
+
+
+
+    def test_read_position_sub_total(self):
+        ws = self.get_worksheet('\\samples\\sample_holdings2.xls')
+        fields = read_fields(ws, 2)
+
+        # read a normal position
+        position = {}
+        initialize_position(position)
+        read_position_holding_detail(ws, 3, fields, position)
+        read_position_sub_total(ws, 4, fields, position)
+        self.assertEqual(len(position), 21)
+        self.assertEqual(position['sub_total'], 7200000)
+        self.assertEqual(position['market_price_currency'], 'USD')
+        self.assertAlmostEqual(position['market_price'], 96.966)
+        self.assertAlmostEqual(position['market_value'], 6981552)
+
+
+
+    def test_read_position_sub_total2(self):
+        ws = self.get_worksheet('\\samples\\sample_holdings4.xls')
+        fields = read_fields(ws, 2)
+
+        # read an All section position
+        position = {}
+        initialize_position(position)
+        read_position_holding_detail(ws, 600, fields, position)
+        read_position_sub_total(ws, 601, fields, position)
+        self.assertEqual(len(position), 17)
+        self.assertEqual(position['sub_total'], 2000000)
+        self.assertEqual(position['market_price_currency'], 'USD')
+        self.assertAlmostEqual(position['market_price'], 97.4179)
+        self.assertAlmostEqual(position['market_value'], 1948358)
+
+
+
+    def test_read_position_available_balance(self):
+        ws = self.get_worksheet('\\samples\\sample_holdings3.xls')
+        fields = read_fields(ws, 2)
+
+        position = {}
+        read_position_available_balance(ws, 11, fields, position)
+        self.assertEqual(position['available_balance'], 3000000)
+
+
+
+    def test_read_position(self):
+        ws = self.get_worksheet('\\samples\\sample_holdings3.xls')
+        fields = read_fields(ws, 2)
+
+        position = {}
+        row = read_position(ws, 3, fields, position)
+        self.assertEqual(row, 6)
+
+        position = {}
+        row = read_position(ws, 19, fields, position)
+        self.assertEqual(row, 25)
+
+        self.validate_position_fields(position)
+
+
+
+    def test_validate_position(self):
+        ws = self.get_worksheet('\\samples\\sample_holdings3.xls')
+        fields = read_fields(ws, 2)
+
+        position = {}
+        row = read_position(ws, 3, fields, position)
+        try:
+            validate_position(position)
+        except:
+            self.fail('position validation failed')
+
+        position = {}
+        row = read_position(ws, 19, fields, position)
+        try:
+            validate_position(position)
+        except:
+            self.fail('position 2 validation failed')
+
+
+
+    def test_grand_total(self):
+        ws = self.get_worksheet('\\samples\\sample_holdings1.xls')
+        fields = read_fields(ws, 2)
+
+        self.assertTrue(is_grand_total(ws, 93))
+        try:
+            x = read_grand_total(ws, 93)
+            self.assertTrue(x > 0)
+        except:
+            self.fail('read grand total failed')
+
+
+
+    def test_read_holdings(self):
+        ws = self.get_worksheet('\\samples\\sample_holdings2.xls')
+        fields = read_fields(ws, 2)
+        port_values = {}
+        try:
+            x = read_holdings(ws, 3, port_values, fields)
+            self.assertTrue(x is None)
+            self.verify_holdings(port_values['holdings'])
+        except:
+            self.fail('read_holdings() failed')
+
+
+
+    def test_read_holdings2(self):
+        ws = self.get_worksheet('\\samples\\sample_holdings4.xls')
+        fields = read_fields(ws, 2)
+        port_values = {}
+        try:
+            x = read_holdings(ws, 3, port_values, fields)
+            self.assertAlmostEqual(x, 85015806879.97)
+            self.verify_holdings2(port_values['holdings'])
+        except:
+            self.fail('read_holdings() failed')
+
+
+
+    def test_read_bochk(self):
+        filename = get_current_path() + '\\samples\\sample_holdings2.xls'
+        port_values = {}
+        read_bochk(filename, port_values)
+        self.verify_holdings(port_values['holdings'])
+
+
+
+    def test_read_bochk2(self):
+        filename = get_current_path() + '\\samples\\sample_holdings4.xls'
+        port_values = {}
+        read_bochk(filename, port_values)
+        self.verify_holdings2(port_values['holdings'])
+
+
+
+    def validate_position_fields(self, position):
+        """
+        Fields in a normal position.
+        """
+        fields = ['generation_date', 'statement_date', 'account_name',
+                    'account_number', 'market_code', 'market_name',
+                    'security_id_type', 'security_id', 'security_name',
+                    'quantity_type', 'market_price_currency',
+                    'market_price', 'market_value', 'exchange_currency_pair',
+                    'exchange_rate', 'equivalent_currency',
+                    'equivalent_market_value', 'settled_units', 'pending_receipt',
+                    'pending_delivery', 'sub_total', 'available_balance']
+
+        self.assertEqual(len(position), len(fields))
+        for fld in position.keys():
+            self.assertTrue(fld in fields)
+
+
+
+    def validate_position_fields_All_section(self, position):
+        """
+        Fields in a position in All section, see samples/sample_holdings4.xls
+        """
+        fields = ['generation_date', 'statement_date', 'account_name',
+                    'account_number', 'market_code', 'market_name',
+                    'security_id_type', 'security_id', 'security_name',
+                    'quantity_type', 'market_price_currency',
+                    'market_price', 'market_value', 'settled_units', 
+                    'pending_receipt', 'pending_delivery', 'sub_total', 
+                    'available_balance']
+        self.assertEqual(len(position), len(fields))
+        for fld in position.keys():
+            self.assertTrue(fld in fields)
+
+
+
+    def verify_holdings(self, holdings):
+        """
+        For samples/sample_holdings2.xls
+        """
+        self.assertEqual(len(holdings), 27)
+        self.validate_position1(holdings[0])
+        self.validate_position2(holdings[17])
+        self.validate_position3(holdings[26])
+
+
+
+    def verify_holdings2(self, holdings):
+        """
+        For samples/sample_holdings4.xls
+        """
+        self.assertEqual(len(holdings), 284)
+        self.validate_position01(holdings[0])
+        self.validate_position02(holdings[198])
+        self.validate_position03(holdings[283])
+
+
+
+    def validate_position1(self, position):
+        """
+        For first position in samples/sample_holdings2.xls
+        """
+        self.validate_position_fields(position)
+        self.assertEqual(position['statement_date'], datetime(2016,11,16))
+        self.assertEqual(position['account_name'], 'MAPLES TRUSTEE S(CY)LTD-CHINA L F TT-CONCORD F INV')
+        self.assertEqual(position['security_id'], 'US09681MAC29')
+        self.assertEqual(position['sub_total'], 7200000)
+        self.assertEqual(position['market_price_currency'], 'USD')
+        self.assertAlmostEqual(position['market_price'], 96.966)
+        self.assertAlmostEqual(position['market_value'], 6981552)
+
+
+
+    def validate_position2(self, position):
+        """
+        For the 18th position in samples/sample_holdings2.xls
+        """
+        self.validate_position_fields(position)
+        self.assertEqual(position['statement_date'], datetime(2016,11,16))
+        self.assertEqual(position['account_name'], 'MAPLES TRUSTEE S(CY)LTD-CHINA L F TT-CONCORD F INV')
+        self.assertEqual(position['security_id'], 'XS1422790615')
+        self.assertEqual(position['settled_units'], 7500000)
+        self.assertEqual(position['sub_total'], 0)
+        self.assertEqual(position['available_balance'], 0)
+        self.assertAlmostEqual(position['market_price'], 100.761)
+
+
+
+    def validate_position3(self, position):
+        """
+        For the last (27th) position in samples/sample_holdings2.xls
+        """
+        self.validate_position_fields(position)
+        self.assertEqual(position['statement_date'], datetime(2016,11,16))
+        self.assertEqual(position['security_id'], 'XS1509266026')
+        self.assertEqual(position['settled_units'], 5000000)
+        self.assertEqual(position['sub_total'], 5000000)
+        self.assertEqual(position['available_balance'], 5000000)
+        self.assertAlmostEqual(position['market_price'], 99.912)
+        self.assertEqual(position['exchange_currency_pair'], 'USD/USD')
+        self.assertAlmostEqual(position['equivalent_market_value'], 4995600)
+
+
+
+    def validate_position01(self, position):
+        """
+        For first position in samples/sample_holdings4.xls
+        """
+        self.validate_position_fields(position)
+        self.assertEqual(position['generation_date'], datetime(2016,7,7))
+        self.assertEqual(position['account_name'], 'CLT-CLI HK BR (CLS A-HK)TRUST FUND (SUB-FUND-BOND)')
+        self.assertEqual(position['security_id'], 'FR0013101599')
+        self.assertEqual(position['sub_total'], 400000)
+        self.assertEqual(position['market_price_currency'], 'USD')
+        self.assertAlmostEqual(position['market_price'], 107.503)
+        self.assertAlmostEqual(position['market_value'], 430012)
+
+
+
+    def validate_position02(self, position):
+        """
+        For the last(199th) holding position in samples/sample_holdings4.xls
+        """
+        self.validate_position_fields(position)
+        self.assertEqual(position['statement_date'], datetime(2016,7,6))
+        self.assertEqual(position['account_name'], 'CLT-CLI OVERSEAS TRUST FD (CAPITAL) (SUB-FD-BOND)')
+        self.assertEqual(position['security_id'], 'USY32358AA46')
+        self.assertEqual(position['settled_units'], 3000000)
+        self.assertEqual(position['sub_total'], 3000000)
+        self.assertEqual(position['available_balance'], 3000000)
+        self.assertEqual(position['equivalent_currency'], 'HKD')
+        self.assertAlmostEqual(position['exchange_rate'], 7.7574)
+        self.assertAlmostEqual(position['market_price'], 109.292)
+
+
+
+    def validate_position03(self, position):
+        """
+        For the last (283th) position in All section, in samples/sample_holdings4.xls
+        """
+        self.validate_position_fields_All_section(position)
+        self.assertEqual(position['statement_date'], datetime(2016,7,6))
+        self.assertEqual(position['security_id'], 'US78490FMJ56')
+        self.assertEqual(position['market_code'], 'USY')
+        self.assertEqual(position['settled_units'], 1000000)
+        self.assertEqual(position['sub_total'], 1000000)
+        self.assertEqual(position['available_balance'], 1000000)
+        self.assertAlmostEqual(position['market_price'], 96.11)
+        self.assertEqual(position['market_value'], 961100)
